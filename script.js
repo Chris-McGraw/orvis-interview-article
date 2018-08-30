971423e8621f594ea7e5ef44d545ddb29bbbcc76
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-08-29 21:09:48 -0400

    index-update-5 / style-update-5 / script-update-2
    initial addition of progressive image loading functionality

--- script.js
+++ script.js
@@ -1,65 +1,56 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var mainImgContainer = document.getElementById("main-img-container");
+  var mainImg = document.getElementById("main-img");
   var subImg0 = document.getElementById("sub-img-0");
   var subImg1 = document.getElementById("sub-img-1");
 
-  var bridgeImg = 'url("https://bytebucket.org/orvis_ecommerce/interview-packet/raw/10cb207462ee5c1e72eba25b3077b80641a12b5c/images/photo07.jpg")';
-  var countryImg = 'url("https://bytebucket.org/orvis_ecommerce/interview-packet/raw/10cb207462ee5c1e72eba25b3077b80641a12b5c/images/photo08.jpg")';
-  var brickImg = 'url("https://bytebucket.org/orvis_ecommerce/interview-packet/raw/10cb207462ee5c1e72eba25b3077b80641a12b5c/images/photo04.jpg")';
+  var bridgeImg = "https://bytebucket.org/orvis_ecommerce/interview-packet/raw/10cb207462ee5c1e72eba25b3077b80641a12b5c/images/photo07.jpg";
+  var countryImg = "https://bytebucket.org/orvis_ecommerce/interview-packet/raw/10cb207462ee5c1e72eba25b3077b80641a12b5c/images/photo08.jpg";
+  var brickImg = "https://bytebucket.org/orvis_ecommerce/interview-packet/raw/10cb207462ee5c1e72eba25b3077b80641a12b5c/images/photo04.jpg";
 
-  mainImgContainer.style.backgroundImage = bridgeImg;
-  subImg0.style.backgroundImage = countryImg;
-  subImg1.style.backgroundImage = brickImg;
-
+  setTimeout(function() {
+    mainImg.src = bridgeImg;
+    subImg0.src = countryImg;
+    subImg1.src = brickImg;
+  }, 0);
 
   function swapImages(subImg) {
-    if(subImg.style.backgroundImage === countryImg && mainImgContainer.style.backgroundImage === bridgeImg) {
-      subImg.style.backgroundImage = bridgeImg;
-
-      mainImgContainer.style.backgroundImage = countryImg;
+    if(subImg.src === countryImg && mainImg.src === bridgeImg) {
+      subImg.src = bridgeImg;
+      mainImg.src = countryImg;
     }
-    else if(subImg.style.backgroundImage === bridgeImg && mainImgContainer.style.backgroundImage === countryImg) {
-      subImg.style.backgroundImage = countryImg;
-
-      mainImgContainer.style.backgroundImage = bridgeImg;
+    else if(subImg.src === bridgeImg && mainImg.src === countryImg) {
+      subImg.src = countryImg;
+      mainImg.src = bridgeImg;
     }
 
-
-    else if(subImg.style.backgroundImage === countryImg && mainImgContainer.style.backgroundImage === brickImg) {
-      subImg.style.backgroundImage = brickImg;
-
-      mainImgContainer.style.backgroundImage = countryImg;
+    else if(subImg.src === countryImg && mainImg.src === brickImg) {
+      subImg.src = brickImg;
+      mainImg.src = countryImg;
     }
-    else if(subImg.style.backgroundImage === brickImg && mainImgContainer.style.backgroundImage === countryImg) {
-      subImg.style.backgroundImage = countryImg;
-
-      mainImgContainer.style.backgroundImage = brickImg;
+    else if(subImg.src === brickImg && mainImg.src === countryImg) {
+      subImg.src = countryImg;
+      mainImg.src = brickImg;
     }
 
-
-    else if(subImg.style.backgroundImage === brickImg && mainImgContainer.style.backgroundImage === bridgeImg) {
-      subImg.style.backgroundImage = bridgeImg;
-
-      mainImgContainer.style.backgroundImage = brickImg;
+    else if(subImg.src === brickImg && mainImg.src === bridgeImg) {
+      subImg.src = bridgeImg;
+      mainImg.src = brickImg;
     }
-    else if(subImg.style.backgroundImage === bridgeImg && mainImgContainer.style.backgroundImage === brickImg) {
-      subImg.style.backgroundImage = brickImg;
-
-      mainImgContainer.style.backgroundImage = bridgeImg;
+    else if(subImg.src === bridgeImg && mainImg.src === brickImg) {
+      subImg.src = brickImg;
+      mainImg.src = bridgeImg;
     }
 
-
-    else if(subImg.style.backgroundImage === brickImg && mainImgContainer.style.backgroundImage === countryImg) {
-      subImg.style.backgroundImage = countryImg;
-
-      mainImgContainer.style.backgroundImage = brickImg;
+    else if(subImg.src === brickImg && mainImg.src === countryImg) {
+      subImg.src = countryImg;
+      mainImg.src = brickImg;
     }
-    else if(subImg.style.backgroundImage === countryImg && mainImgContainer.style.backgroundImage === brickImg) {
-      subImg.style.backgroundImage = brickImg;
-
-      mainImgContainer.style.backgroundImage = countryImg;
+    else if(subImg.src === countryImg && mainImg.src === brickImg) {
+      subImg.src = brickImg;
+      mainImg.src = countryImg;
     }
   }
+
 
 
   subImg0.onclick = function() {
